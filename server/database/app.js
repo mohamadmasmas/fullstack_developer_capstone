f48cac564f7f527047226e3d53caeba5cf0669f3
Mohamad Masmas <100225589+mohamadmasmas@users.noreply.github.com>
2024-08-17 22:02:18 +0300

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -1,18 +1,18 @@
+/*jshint esversion: 8 */
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
 mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
-
 
 const Reviews = require('./review');
 
@@ -28,12 +28,11 @@ try {
   
 } catch (error) {
   res.status(500).json({ error: 'Error fetching documents' });
-}
-
+};
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -93,8 +92,8 @@ app.get('/fetchDealer/:id', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0]['id']+1;
 
   const review = new Reviews({
 		"id": new_id,
