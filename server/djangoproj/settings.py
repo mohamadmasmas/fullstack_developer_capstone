ec02e5cd38b6475b6a8fab6ad722711dde5c75ca
Mohamad Masmas <100225589+mohamadmasmas@users.noreply.github.com>
2024-08-17 23:47:05 +0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -11,13 +11,15 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-'https://masmasmohama-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/'
-                ]
+    'https://masmasmohama-8000.theianext'\
+    '-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
 CSRF_TRUSTED_ORIGINS = [
-'https://masmasmohama-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/',
-'https://masmasmohama-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-'https://masmasmohama-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-]
+    'https://masmasmohama-8000.theianext-1-labs-'\
+    'prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/',
+    'https://masmasmohama-8000.theianext-1-labs-prod-'\
+    'misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://masmasmohama-8000.theiadockernext-0-labs-'\
+    'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -116,7 +118,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/static'),
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
