1a01677d3f02be44a47a06584c1e1151ae337991
Mohamad Masmas <100225589+mohamadmasmas@users.noreply.github.com>
2024-08-17 23:15:17 +0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -10,11 +10,12 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS= ['localhost', 
-                'https://masmasmohama-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS= ['https://masmasmohama-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/', 
-                       'https://masmasmohama-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
-                       'https://masmasmohama-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost',\
+    'https://masmasmohama-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS = \
+['https://masmasmohama-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/',\
+'https://masmasmohama-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', \
+'https://masmasmohama-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -45,10 +46,10 @@ ROOT_URLCONF = 'djangoproj.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',\
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static'),
-            os.path.join(BASE_DIR,'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
             os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
@@ -74,20 +75,24 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':\
+        'django.contrib.auth.password_validation'\
+        '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':\
+        'django.contrib.auth.'\
+        'password_validation.MinimumLengthValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':\
+        'django.contrib.auth.'\
+        'password_validation.CommonPasswordValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':\
+        'django.contrib.auth.'\
+        'password_validation.NumericPasswordValidator',
     },
 ]
 
